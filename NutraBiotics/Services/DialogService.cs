9b92b4d449506ee98bf59ef5b7dd2cdf93730fcb
Juan Carlos Zuluaga <jzuluaga55@gmail.com>
2017-07-18 13:42:20 -0500

    Login UI

---
+++ NutraBiotics/Services/DialogService.cs
@@ -0,0 +1,17 @@
+﻿namespace NutraBiotics.Services
+{
+    using System.Threading.Tasks;
+
+	public class DialogService
+    {
+		public async Task ShowMessage(string title, string message)
+		{
+			await App.Current.MainPage.DisplayAlert(title, message, "Aceptar");
+		}
+
+		public async Task<bool> ShowConfirm(string title, string message)
+		{
+			return await App.Current.MainPage.DisplayAlert(title, message, "Si", "No");
+		}
+    }
+}
--- NutraBiotics/ViewModels/LoginViewModel.cs
+++ NutraBiotics/ViewModels/LoginViewModel.cs
@@ -1,10 +1,136 @@
-﻿using System;
-namespace NutraBiotics.ViewModels
+﻿namespace NutraBiotics.ViewModels
 {
-    public class LoginViewModel
+    using System.ComponentModel;
+    using System.Windows.Input;
+    using GalaSoft.MvvmLight.Command;
+    using Services;
+
+    public class LoginViewModel : INotifyPropertyChanged
     {
+        #region Events
+        public event PropertyChangedEventHandler PropertyChanged;
+        #endregion
+
+        #region Attributes
+        DialogService dialogService;
+		string email;
+		string password;
+		bool isRunning;
+		bool isEnabled;
+		bool isRemembered;
+		#endregion
+
+		#region Properties
+		public string Email
+		{
+			set
+			{
+				if (email != value)
+				{
+					email = value;
+					PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Email)));
+				}
+			}
+			get
+			{
+				return email;
+			}
+		}
+
+		public string Password
+		{
+			set
+			{
+				if (password != value)
+				{
+					password = value;
+                    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Password)));
+				}
+			}
+			get
+			{
+				return password;
+			}
+		}
+
+		public bool IsRunning
+		{
+			set
+			{
+				if (isRunning != value)
+				{
+					isRunning = value;
+                    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(IsRunning)));
+				}
+			}
+			get
+			{
+				return isRunning;
+			}
+		}
+
+		public bool IsEnabled
+		{
+			set
+			{
+				if (isEnabled != value)
+				{
+					isEnabled = value;
+                    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(IsEnabled)));
+				}
+			}
+			get
+			{
+				return isEnabled;
+			}
+		}
+
+		public bool IsRemembered
+		{
+			set
+			{
+				if (isRemembered != value)
+				{
+					isRemembered = value;
+                    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(IsRemembered)));
+				}
+			}
+			get
+			{
+				return isRemembered;
+			}
+		}
+        #endregion
+
+        #region Constructors
         public LoginViewModel()
         {
+            dialogService = new DialogService();
+            IsEnabled = true;
         }
+        #endregion
+
+        #region Commands
+        public ICommand LoginCommand
+        {
+            get { return new RelayCommand(Login); }
+        }
+
+        async void Login()
+        {
+            if (string.IsNullOrEmpty(Email))
+            {
+                await dialogService.ShowMessage("Error", "Debes ingresar un email.");
+                return;
+            }
+
+			if (string.IsNullOrEmpty(Password))
+			{
+				await dialogService.ShowMessage("Error", "Debes ingresar una contraseña.");
+				return;
+			}
+		}
+        #endregion
+
     }
 }
